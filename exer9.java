d07fc0bb2618d5de6836198b5b87474579d15370
b4rb4r4-c3cim <barbara.cecim@gmail.com>
2023-01-19 03:11:49 -0300

    array

---
+++ exer9.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class exer9 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[10];
+        int[] vetorC = new int[10];
+
+        for(int i=0; i < vetorA.length; i++){
+            System.out.println("Entre com os valores do vetor A: ");
+            vetorA[i] = scan.nextInt();
+        }
+
+        for(int i=0; i < vetorB.length; i++){
+            System.out.println("Entre com os valores do vetor B: ");
+            vetorB[i] = scan.nextInt();
+        }
+
+        System.out.println("Soma vetor A e B (vetor C): ");
+        for(int i=0; i < vetorC.length; i++){
+            vetorC[i] = vetorA[i] * vetorB[i];
+            System.out.println("Vetor C: ");
+            System.out.println(vetorC[i] + " ");
+        }
+    }
+}
